00f61f4fcafd42d5a8d32fdc04d1ce004ae2889c
Douglas Petrin <editor@douglaspetrin.com>
2018-10-31 19:08:43 -0300

    up

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,11 +1,13 @@
 import React , { Component } from 'react';
 import api from '../../services/api';
 import './styles.css'
+import { Link } from 'react-router-dom';
 
 export default class Main extends Component {
     state = { // o estado amarzena objeto, ou seja, os dados da api q estou buscando
         products: [],
         productInfo: {},
+        page: 1,
     };
     
     
@@ -13,39 +15,53 @@ export default class Main extends Component {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         
-        const { docs, productInfo } = response.data; //criei duas variaveis pra receber as info dos products e o docs
 
-        this.setState({ products: docs, productInfo}); //aqui ouve a alteracao do state, entao pegou o valor de docs = 10
+        //... rest operator
+        const { docs, ...productInfo } = response.data; //criei duas variaveis pra receber as info dos products e o docs
+
+        this.setState({ products: docs, productInfo, page}); //aqui ouve a alteracao do state, entao pegou o valor de docs = 10
     };
     
 
     prevPage = () => {
+        const { page, productInfo } = this.state;
 
-    }
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
+    };
 
     nextPage = () => {
+        const { page, productInfo } = this.state;
 
-    }
+        if (page === productInfo.pages) return; //se a pagina atual for igual a pagina atual, retornar.
+
+        const pageNumber = page + 1; //aqui cria uma variavel pra contar as paginas
+
+        this.loadProducts(pageNumber);
+
+    };
 
     render() {
 
-        const { products } = this.state; //dessa maneira product vai ta recebendo products do this.state e nao preciso mais chamar this.state abaixo
+        const { products, page, productInfo } = this.state; //dessa maneira product vai ta recebendo products do this.state e nao preciso mais chamar this.state abaixo
 
         return (
         <div className="product-list"> 
-        {products.map(products => (
-            <article key={products._id}>
-                <strong>{products.title}</strong>
-                <p>{products.description}</p>
-                <a href="">Acessar</a>
+        {products.map(product => (
+            <article key={product._id}>
+                <strong>{product.title}</strong>
+                <p>{product.description}</p>
+                <Link to={`/products/${product._id}`}>Acessar</Link>
             </article>
-        ))}
+        ))};
             <div className="actions">
-            <button onClick={this.prevPage}> Anterior </button>
-            <button onClick={this.nextPage}> Próximo </button>
+            <button disabled={page === 1} onClick={this.prevPage}> Anterior </button>
+            <button disabled={ page === productInfo.pages } onClick={this.nextPage}> Próximo </button>
 
             </div> 
         </div> 
